860cbc07efb3242c652abd2a4079693064d13e00
Shaun Bresnahan <Shaun.Bresnahan@ukho.gov.uk>
2020-11-27 13:35:02 +0000

    update

--- main.tf
+++ main.tf
@@ -71,8 +71,8 @@ data "azurerm_route_table" "main" {
 resource "azurerm_route" "main" {
   provider            = azurerm.spoke
   name                = var.spokeroute[count.index]
-  resource_group_name = data.azurerm_resource_group.main.name
-  route_table_name    = data.azurerm_route_table.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  route_table_name    = azurerm_route_table.main.name
   address_prefix      = var.spokeprefix[count.index]
   next_hop_type       = var.hop[count.index]
   count               = length(var.spokeroute)
