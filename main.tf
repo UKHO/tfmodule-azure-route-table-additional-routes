d9eb8f7d4d83afe87f9b3d56f8827681dc1a803a
Shaun Bresnahan <Shaun.Bresnahan@ukho.gov.uk>
2020-11-27 12:00:12 +0000

    Updated module

--- main.tf
+++ main.tf
@@ -1,66 +1,56 @@
-locals {
-  subscription_id     = "5739fe4c-408e-45ba-941e-301ae4525146"
-  hub_subscription_id = "282900b8-5415-4137-afcc-fd13fe9a64a7"
-}
+#locals {
+#  subscription_id     = "5739fe4c-408e-45ba-941e-301ae4525146"
+#  hub_subscription_id = "282900b8-5415-4137-afcc-fd13fe9a64a7"
+#}
+
+#provider "azurerm" {
+#  features {}
+#  version = "=2.20.0"
+#}
 
 provider "azurerm" {
-  features {}
-  version = "=2.20.0"
-
-}
-
-provider "azurerm" {
-  version = "=2.20.0"
-  features {}
+  #version = "=2.20.0"
+  #features {}
   alias = "spoke"
-  subscription_id = local.subscription_id  
+  #subscription_id = local.subscription_id  
 }
 
 provider "azurerm" {
-  version = "=2.20.0"
-  features {}
+  #version = "=2.20.0"
+  #features {}
   alias = "hub"
-  subscription_id = local.hub_subscription_id
+  #subscription_id = local.hub_subscription_id
 }
 
-resource "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "main" {
   provider = azurerm.spoke
   name     = var.spokerg
-  location = "uksouth"
+  #location = "uksouth"
 }
 
 resource "azurerm_route_table" "main" {
+  provider                      = azurerm.spoke
   name                          = var.routetable
-  location                      = azurerm_resource_group.main.location
-  resource_group_name           = azurerm_resource_group.main.name
+  location                      = data.azurerm_resource_group.main.location
+  resource_group_name           = data.azurerm_resource_group.main.name
   disable_bgp_route_propagation = false
   
   lifecycle { ignore_changes = [tags] }
 }
 
-#data "azurerm_subnet" "main" {
-#  count                = length(var.subnets)
-#  provider             = azurerm.spoke
-#  name                 = var.subnets[count.index].name
-#  resource_group_name  = var.spokerg
-#  virtual_network_name = local.vnet_name
-#}
-#
-#resource "azurerm_subnet_route_table_association" "main" {
-#  count          = length(var.subnets)
-#  provider       = azurerm.spoke
-#  subnet_id      = data.azurerm_subnet.main[count.index].id
-#  route_table_id = azurerm_route_table.main.id
-#}
+data "azurerm_subnet" "main" {
+  count                = length(var.subnets)
+  provider             = azurerm.spoke
+  name                 = var.subnets[count.index].name
+  resource_group_name  = var.spokerg
+  virtual_network_name = local.vnet_name
+}
 
-resource "azurerm_route" "main" {
-  provider            = azurerm.spoke
-  name                = var.spokeroute[count.index]
-  resource_group_name = azurerm_resource_group.main.name
-  route_table_name    = azurerm_route_table.main.name
-  address_prefix      = var.spokeprefix[count.index]
-  next_hop_type       = var.hop[count.index]
-  count               = length(var.spokeroute)
+resource "azurerm_subnet_route_table_association" "main" {
+  count          = length(var.subnets)
+  provider       = azurerm.spoke
+  subnet_id      = data.azurerm_subnet.main[count.index].id
+  route_table_id = azurerm_route_table.main.id
 }
 
 data "azurerm_resource_group" "hub" {
@@ -72,6 +62,16 @@ data "azurerm_route_table" "main" {
   provider            = azurerm.hub
   name                = var.hubrt
   resource_group_name = data.azurerm_resource_group.hub.name
+}
+
+resource "azurerm_route" "main" {
+  provider            = azurerm.spoke
+  name                = var.spokeroute[count.index]
+  resource_group_name = data.azurerm_resource_group.main.name
+  route_table_name    = data.azurerm_route_table.main.name
+  address_prefix      = var.spokeprefix[count.index]
+  next_hop_type       = var.hop[count.index]
+  count               = length(var.spokeroute)
 }
 
 resource "azurerm_route" "main1" {
